ed6122ffffb2486764437f7440c849ba4d94ccce
khaoularj <khaoula.raja1@gmail.com>
2024-06-18 15:45:34 +0000

    Create a new user

---
+++ controllers/UsersController.js
@@ -0,0 +1,96 @@
+import request from 'supertest';
+import { expect } from 'chai';
+import dbClient from '../../utils/db'; // Assuming dbClient is correctly implemented
+
+describe('UserController', () => {
+  const mockUser = {
+    email: 'beloxxi@blues.com',
+    password: 'melody1982',
+  };
+
+  before(function (done) {
+    this.timeout(10000);
+    dbClient.usersCollection()
+      .then((usersCollection) => {
+        usersCollection.deleteMany({ email: mockUser.email })
+          .then(() => done())
+          .catch((deleteErr) => done(deleteErr));
+      })
+      .catch((connectErr) => done(connectErr));
+  });
+
+  describe('POST /users', () => {
+    it('should fail when there is no email and there is password', function (done) {
+      this.timeout(5000);
+      request('/users')
+        .post('')
+        .send({
+          password: mockUser.password,
+        })
+        .expect(400)
+        .end((err, res) => {
+          if (err) {
+            return done(err);
+          }
+          expect(res.body).to.deep.equal({ error: 'Missing email' });
+          done();
+        });
+    });
+
+    it('should fail when there is email and there is no password', function (done) {
+      this.timeout(5000);
+      request('/users')
+        .post('')
+        .send({
+          email: mockUser.email,
+        })
+        .expect(400)
+        .end((err, res) => {
+          if (err) {
+            return done(err);
+          }
+          expect(res.body).to.deep.equal({ error: 'Missing password' });
+          done();
+        });
+    });
+
+    it('should succeed when the new user has a password and email', function (done) {
+      this.timeout(5000);
+      request('/users')
+        .post('')
+        .send({
+          email: mockUser.email,
+          password: mockUser.password,
+        })
+        .expect(201)
+        .end((err, res) => {
+          if (err) {
+            return done(err);
+          }
+          expect(res.body.email).to.equal(mockUser.email);
+          expect(res.body.id).to.be.a('string').that.is.not.empty;
+          done();
+        });
+    });
+
+    it('should fail when the user already exists', function (done) {
+      this.timeout(5000);
+      request('/users')
+        .post('')
+        .send({
+          email: mockUser.email,
+          password: mockUser.password,
+        })
+        .expect(400)
+        .end((err, res) => {
+          if (err) {
+            return done(err);
+          }
+          expect(res.body).to.deep.equal({ error: 'Already exist' });
+          done();
+        });
+    });
+  });
+
+});
+
